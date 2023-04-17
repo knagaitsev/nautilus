a93a2b9cb94855edc9e6cad1130619682793d1d2
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2023-04-17 12:24:34 -0500

    fix build normal script

--- scripts/build_normal.sh
+++ scripts/build_normal.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export NAUT_ENABLE_INTS=1; make clean ; make -j ; make bitcode ; make isoimage ; make uImage
-=======
-export NAUT_ENABLE_INTS=1 && make clean && make -j && make bitcode && make final_no_timing && make isoimage && make uImage
->>>>>>> fdf6b0a8 (add easier way to pick out desired benchmark)
+export NAUT_ENABLE_INTS=1 && make clean && make -j && make bitcode && make isoimage && make uImage
