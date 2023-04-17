10bbe38ba1f8029c3ac6f285cab8fd24dbbefd2b
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2023-04-17 12:19:18 -0500

    add easier way to pick out desired benchmark

--- scripts/build_normal.sh
+++ scripts/build_normal.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export NAUT_ENABLE_INTS=1; make clean ; make -j ; make bitcode ; make isoimage ; make uImage
+=======
+export NAUT_ENABLE_INTS=1 && make clean && make -j && make bitcode && make final_no_timing && make isoimage && make uImage
+>>>>>>> fdf6b0a8 (add easier way to pick out desired benchmark)
